12d36bbede97a5d563101790c36114c0eee18d4d
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-13 21:12:03 +0430

    create google translate api

---
+++ google_translate/translate.py
@@ -0,0 +1,36 @@
+import aiohttp
+from bs4 import BeautifulSoup as bs
+
+
+class GoogleTranslate:
+    """Translate text from https://translate.google.com/m"""
+
+    def __init__(self, tl, text) -> None:
+        self.sl = "auto"
+        self.tl = tl
+        self.text = text
+        self.params = {
+            "sl": self.sl,
+            "tl": self.tl,
+            "q": self.text,
+            "hl": "en"
+        }
+
+    async def translate(self) -> str:
+        url = "https://translate.google.com/m"
+        connector = aiohttp.TCPConnector(limit=50)
+        async with aiohttp.ClientSession(connector=connector) as session:
+            res = await session.get(url=url, params=self.params)
+            result = await res.text()
+
+            # find translate
+            translation = await self.find_translation(result)
+            return translation
+
+
+    async def find_translation(self, res):
+        soup = bs(res, 'html.parser')
+        translation = soup.find_all("div", {"class":"result-container"})[0].text
+        return translation
+
+    
