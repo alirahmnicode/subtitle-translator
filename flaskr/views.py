2b617e52dea99ad1e9d83372fd4f2151624feb7e
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-15 20:58:22 +0430

    send created file

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request 
+from flask import Blueprint, render_template, request, send_file
 
 from subtitle import translate_subtitle
 
 
 bp = Blueprint("views", __name__)
+
 
 @bp.get("/")
 def index():
@@ -14,18 +15,17 @@ def index():
 async def translate_view():
     if request.method == "POST":
         # check if the post request has the file part
-        if 'file' not in request.files:
+        if "file" not in request.files:
             print("ali")
-        file = request.files['file']
+        file = request.files["file"]
         subtitle_text = file.read().decode("utf-8")
 
         # translate subtitle
         new_file = await translate_subtitle(subtitle_text, "fa")
-        # save file
-        with open("a.srt", 'w') as f:
-            f.write(new_file)
 
-        return render_template("subtitle/translate.html")
-    
+        return send_file(
+            new_file, download_name="ali.srt", mimetype="srt", as_attachment=True
+        )
+
     if request.method == "GET":
         return render_template("subtitle/translate.html")
